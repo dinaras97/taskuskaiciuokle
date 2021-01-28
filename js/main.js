289bbaafec376611afacdc2c3d61bbcf80d87b94
Dinaras Klykūnas <dinaras.klykunas@ktu.lt>
2021-01-28 15:58:07 +0200

    Moved all js to one file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,89 @@
-import Game from './game.js'
-import Player from './player.js'
+class Game {
+    constructor() {
+        this.players = []
+        this.round = 0
+    }
+
+    newRound() {
+        this.round++
+        this.players.map(player => player.newRound())
+    }
+
+    addPlayer(player) {
+        this.players.push(player)
+
+        if (this.round !== 0)
+            for (let i = 0; i < this.round; i++)
+                player.newRound()
+    }
+
+    sortPlayers() {
+        this.players.sort((a, b) => {
+            if (a.lastname < b.lastname)
+                return -1
+        })
+
+        this.players.sort((a, b) => {
+            if (a.firstname < b.firstname)
+                return -1
+        })
+
+        this.players.sort((a, b) => {
+            if (a.calculatePoints() > b.calculatePoints())
+                return -1
+        })
+    }
+
+    info() {
+        this.sortPlayers()
+        return this.players
+    }
+}
+
+class Player {
+    constructor(firstname, lastname) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.points = [ ]
+    }
+
+    addPoints(round, points) {
+        this.points[round - 1] += points
+    }
+
+    removePoints(round, points) {
+        this.points[round - 1] -= points
+
+        if (this.points[round - 1] < 0)
+            this.points[round - 1] = 0
+    }
+
+    setPoints(round, points) {
+        this.points[round - 1] = points
+
+        if (this.points[round - 1] < 0)
+            this.points[round - 1] = 0
+    }
+
+    newRound() {
+        this.points.push(0)
+    }
+
+    calculatePoints() {
+        if (this.points.length > 0)
+            return this.points.reduce((a, b) => a + b)
+        
+        return 0
+    }
+
+    info() {
+        return {
+            firstname: this.firstname,
+            lastname: this.lastname,
+            points: this.calculatePoints()
+        }
+    }
+}
 
 let form = document.querySelector('#gameForm')
 let inputs = form.elements
